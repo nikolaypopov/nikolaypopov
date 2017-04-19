91226c820ea59b73bf8e462c1b08fa800601d94c
nikolaypopov <nikolay.popov86@gmail.com>
2017-04-19 11:48:34 -0700

    minor changes for copy.py

--- scripts/copy.py
+++ scripts/copy.py
@@ -3,15 +3,14 @@
 #from subprocess import *
 from subprocess import Popen, PIPE, STDOUT
 
-filename = "somefile.txt"
-pathfrom = "/root/"
-pathto   = "/root/test/"
-runtimes = 5
+filename = "somefile"
+pathfrom = "/path/from/"
+pathto   = "/path/to/"
+runtimes = 10
 
 # Function to execute commands
 def run(cmd):
 	p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-	#output = p.communicate()
 	return p.communicate(), p.returncode
 
 # Function to print success = green, fail = red
@@ -21,9 +20,31 @@ def colorPrint(text, color):
 	if color == "r": # Red print
 		print(("\033[1;31;40m{}\033[0;37;40m").format(text))
 
+def cleanup():
+	# Checking content of the destination folder
+	print "Checking if cleanup is needed ..."
+	ls,_ = run("ls -1 {}".format(pathto))
+
+	deletels = []
+	for line in ls:
+		if line is not None:
+			deletels += line.split("\n")
+			break
+
+	# If file exists we try to delete it
+	if filename in deletels:
+		colorPrint("Found file you trying to copy, initiating cleanup ...", "r")
+		deletefile,returncode = run("rm {}{}".format(pathto, filename))
+		if returncode == 0:
+			colorPrint("Cleanup has finished successfully", "g")
+		else:
+			colorPrint("Cleanup has failed", "r")
+			exit(1)
+	else:
+		colorPrint("Cleanup is not needed", "g")
+
 def execute():
 	# Copy file to directory
-	#print "Copying ", filename, " to " + pathto + " ..."
 	print "Copying {} to {} ...".format(filename, pathto)
 	copyfile,_ = run("cp {}{} {}.".format(pathfrom, filename, pathto))
 
@@ -42,7 +63,7 @@ def execute():
 	if filename in copyls:
 		colorPrint("File was copyed successfully", "g")
 	else:
-		colorPrint("File was not copyed", "r")
+		colorPrint("ERR: File was not copyed", "r")
 		exit(1)
 
 	# Checking diff
@@ -50,7 +71,8 @@ def execute():
 	if returncode == 0:
 		colorPrint("Diff success", "g")
 	else:	
-		colorPrint("Diff fail", "r")
+		colorPrint("ERR: Diff fail", "r")
+		exit(1)
 	
 	# Delete file from directory
 	print "Deleting {} from {} ...".format(filename, pathto)
@@ -68,11 +90,13 @@ def execute():
 	# Printing content of ls -l without printing last element
 	print "Content {}".format(deletels[:-1])
 
-	if filename in deletels:
-		colorPrint("File was not deleted", "r")
-	else:
+	if filename not in deletels:
 		colorPrint("File was deleted successfully", "g")
+	else:
+		colorPrint("ERR: File was not deleted", "r")
+		exit(1)
 
+cleanup()
 print ("Executing copy/delete {} times".format(runtimes))
 times = 0
 while times < runtimes:
